6a282cc1d98d49d2eae28798ae5e20d8a7eebd4f
Miles <tranuyson@gmail.com>
2022-03-03 15:56:34 +0700

    ..

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,17 @@ const axios = require("axios");
 const { newDevice } = require("../controller/devices.controller");
 module.exports = function () {
   console.log("running");
+  effort();
+};
+function effort() {
   axios
     .get(`${process.env.METADATA || "127.0.0.1:33335"}/getAllSchedule`)
     .then(({ data }) => {
       data.forEach((e) => {
         newDevice({ body: { ...e } });
       });
+    })
+    .catch((err) => {
+      effort();
     });
-};
+}
